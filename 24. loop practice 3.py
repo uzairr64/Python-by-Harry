a7c8b4005f87ccc9b448f5090dff8aacd056f287
Uzair Safdar <125918033+uzairr64@users.noreply.github.com>
2023-09-21 00:41:44 +0500

    Add files via upload

---
+++ 24. loop practice 3.py
@@ -0,0 +1,7 @@
+#Python program to calculate the sum of all numbers from 1 to a given number.
+
+g = int(input("enter your ending number: "))
+sum=0
+for i in range(1,g):
+    sum=sum+i
+print(sum)    
