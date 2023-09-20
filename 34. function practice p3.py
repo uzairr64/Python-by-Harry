f5d2db78c09b35537ae295a3d0009048080f095a
Uzair Safdar <125918033+uzairr64@users.noreply.github.com>
2023-09-21 00:44:02 +0500

    Add files via upload

---
+++ 34. function practice p3.py
@@ -0,0 +1,13 @@
+#Define a function that accepts roll number and returns whether the student is present or absent.
+
+def function(roll_number):
+    if roll_number in list:
+        print("Student with" , roll_number , "is Present!")
+
+    else:
+        print("Student with" , roll_number , "is Absent!")
+
+list = [1,2,3,4,5,6,7,8,9]
+roll_number = int(input("enter Roll Number: "))
+
+function(roll_number)
