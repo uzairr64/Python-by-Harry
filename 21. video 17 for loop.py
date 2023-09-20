dd1ac82a07c25c98ef6674ae00a7d1324d7e1948
Uzair Safdar <125918033+uzairr64@users.noreply.github.com>
2023-09-21 00:40:42 +0500

    Add files via upload

---
+++ 21. video 17 for loop.py
@@ -0,0 +1,18 @@
+#name = input("enter Your name! " )
+#print("Entered name is" , name)
+
+#for i in name:
+ #   print(i)
+  #  if i == "z":
+   #     print("second")
+        
+        
+colors = ["Red" , "Green " , "Yellow" , "Blue "]
+for color in colors:
+    print(color)
+    for j in color:
+        print(j)
+        
+        
+for k in range(1,6):
+    print(k)
