260b27cfd7471a4fc5ed3abce966380888b65b85
Uzair Safdar <125918033+uzairr64@users.noreply.github.com>
2023-09-21 00:34:26 +0500

    Add files via upload

---
+++ 01. Video 6 variable and datatypes.py
@@ -0,0 +1,9 @@
+a = complex(1+2)
+b= True
+c = "uzair"
+d = None
+print(a,b,c,d)
+print("The type of a is " , type(a))
+print("The type of b is " , type(b))
+print("The type of c is " , type(c))
+print("The type of d is " , type(d))
