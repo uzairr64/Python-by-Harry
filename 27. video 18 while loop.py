6f647651352c6b0f263ed7ba75f171964b465313
Uzair Safdar <125918033+uzairr64@users.noreply.github.com>
2023-09-21 00:42:27 +0500

    Add files via upload

---
+++ 27. video 18 while loop.py
@@ -0,0 +1,11 @@
+i = 0
+while(i<10):
+    print(i)
+    i = i + 1
+    
+    
+i = int(input("enter starting point: "))
+while(i<20):
+    i = int(input("enter number Ypu want to check:"))
+    print(i)
+print("Done with the loop!")    
