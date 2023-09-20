6fa6bfb469d798d17f28773804c6c4ab1585b7d2
Uzair Safdar <125918033+uzairr64@users.noreply.github.com>
2023-09-21 00:35:19 +0500

    Add files via upload

---
+++ 04. video 9  typecasting p2.py
@@ -0,0 +1,19 @@
+# Expilicist typecasting
+string = "15"
+number = 12
+
+sum= number+int(string)
+print("The sum in Expilicist typecasting is " , sum)
+
+# Implicit typecasting
+
+a = 12
+b = 2.4 
+ 
+print("Sum in Implicit Typecasting is " , a+b) 
+
+print("Type of String is " , type(string))
+print("Type of number is " , type(number))
+print("Type of sum is " , type(sum))
+print("Type of a is " , type(a))
+print("Type of b is " , type(b))
