fcb250a8b3e7b80a2e57a647e1256a7aa5c8ff6b
Uzair Safdar <125918033+uzairr64@users.noreply.github.com>
2023-09-21 00:38:41 +0500

    Add files via upload

---
+++ 14. if else practice p3.py
@@ -0,0 +1,11 @@
+length = int(input("enter length: "))
+breadth = int(input("enter breadth "))
+
+if length == breadth:
+    print("Your Rectangle is also a Square!")
+    
+elif breadth > length:
+    print("Your Rectangle is not a Square!")
+    
+else:
+    print("Your Rectangle is not a Square!")
