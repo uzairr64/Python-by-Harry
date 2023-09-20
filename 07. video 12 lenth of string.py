eae2b44c0bd9249db643f65a44940f85ecd10825
Uzair Safdar <125918033+uzairr64@users.noreply.github.com>
2023-09-21 00:36:13 +0500

    Add files via upload

---
+++ 07. video 12 lenth of string.py
@@ -0,0 +1,17 @@
+name = "uzair"
+ran = "Pakistan"
+print(name[0:5])
+
+
+length_of_name = len(name)
+
+print("length of name is " , len(name))
+
+print(name[0:5])
+print(name[0:-3])
+print(name[:-1])
+print(len(ran))
+
+print(ran[-7:-5])
+print(name[-4:-2])
+
