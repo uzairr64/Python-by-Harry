050c80c334b580604df3abf37b4a97705a2fd86a
Uzair Safdar <125918033+uzairr64@users.noreply.github.com>
2023-09-20 09:05:44 +0500

    Add files via upload

---
+++ 6. video 11 strings in python.py
@@ -0,0 +1,20 @@
+name = "Uzair"
+friend = "Rohan"
+anotherfriend = "Ahsan"
+
+random = """He said, I am Uzair
+My father name is Muhammad Safdar
+I am 20 Years Old"""
+print(random)
+
+print("hello," , name)
+
+print("First alphabet of name is " , name[0])
+print("Second alphabet of name is " , name[1])
+print("Third alphabet of name is " , name[2])
+print("Fourth alphabet of name is " , name[3])
+print("Fifth alphabet of name is " , name[4])
+
+print("\nNOW USING FOR LOOP")
+for character in name:
+      print(character)
