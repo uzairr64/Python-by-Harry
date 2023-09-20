5e3c220ddb71ca2ab8fdfb3c26c7669151cc9ba0
Uzair Safdar <125918033+uzairr64@users.noreply.github.com>
2023-09-21 00:38:27 +0500

    Add files via upload

---
+++ 13. if else practice p2.py
@@ -0,0 +1,10 @@
+age = int(input("enter your age: "))
+
+if age > 18:
+    print("Your age is: " , age , " You are eligible for Driving")
+    
+elif age == 18:
+    print("Come to office ")
+    
+else:
+     print("Your age is " , age , " You are uneligible for Driving")        
